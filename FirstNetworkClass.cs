a007d76548ae0f275f282bf490b41ec46c46bcf7
jayasimha <jayasimhajay1994@gmail.com>
2018-03-07 19:50:07 +0530

    Socket Programming

---
+++ FirstNetworkClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientNetworkProject
+{
+    class FirstNetworkClass
+    {
+        static Socket sck;
+        static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            try
+            {
+                sck.Connect(localEndPoint);
+                Console.WriteLine("Connected");
+            }
+            catch
+            {
+                Console.WriteLine("unable to connect to the end point!\r\n");
+                Main(args);
+            }
+            Console.Write("Enter text: ");
+            string text = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            sck.Send(data);
+            Console.WriteLine("Data Sent!\r\n");
+            Console.WriteLine("Press any key to continue...");
+            sck.Close();
+            Console.Read();
+        }
+    }
+}
---
+++ Second.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace NetworkProject
+{
+    class Second
+    {
+        static void Main(string[] args)
+        {
+            //Socket listener, connector, acc;
+            //listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //connector = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //listener.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"),1994));// or (0,1994) which accepts all Ipaddress
+            //listener.Listen(0);
+            //new Thread(() =>
+            //{
+            //    acc = listener.Accept();
+            //    Console.WriteLine("Connected");
+            //}).Start();
+            //connector.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1994));
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1994));
+            sck.Listen(0);
+            Socket acc = sck.Accept();
+            byte[] buffer = Encoding.ASCII.GetBytes("Hello");
+            acc.Send(buffer, 0, buffer.Length, 0);
+            buffer = new byte[255];
+            int rec = acc.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Received: {0}", Encoding.ASCII.GetString(buffer));
+            sck.Close();
+            acc.Close();
+
+            Console.Read();
+        }
+    }
+}
